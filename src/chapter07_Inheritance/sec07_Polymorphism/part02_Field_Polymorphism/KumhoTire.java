c193f30bee3d566721a7cd13798a400b371a4c03
defectio <defectio916@gmail.com>
2025-03-04 16:46:39 +0900

    다형성 - 필드다형셩

--- src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/KumhoTire.java
+++ src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/KumhoTire.java
@@ -1,11 +1,13 @@
 package chapter07_Inheritance.sec07_Polymorphism.part02_Field_Polymorphism;
 
 public class KumhoTire extends Tire {
-	//필드 -> 부모의 필드 사용
-	
-	//생성자
+
+	/**
+	 * 생성자 - super() 로 부모 생성자 호출
+	 * @param location
+	 * @param maxRotation
+	 */
 	public KumhoTire(String location, int maxRotation) {
-		//super() 로 부모 생성자 호출
 		super(location, maxRotation);
 	}	
 	
@@ -16,12 +18,12 @@ public class KumhoTire extends Tire {
 	public boolean roll() {
 		++accumulatedRotation;		
 		if(accumulatedRotation<maxRotation) {
-			System.out.println(location + " KumhoTire 수명: " + (maxRotation-accumulatedRotation) + "회");
+			System.out.println(location + " KumhoTire 수명: " + (maxRotation - accumulatedRotation) + "회");
 			return true;
 		} else {
 			System.out.println("*** " + location + " KumhoTire 펑크 ***");
 			return false;
 		}
-	} //end roll()
+	}
 	
-} //end class
+}
--- src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/Tire.java
+++ src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/Tire.java
@@ -1,12 +1,11 @@
 package chapter07_Inheritance.sec07_Polymorphism.part02_Field_Polymorphism;
 
 public class Tire {
-	//필드
-	public int maxRotation;     				//최대회전수(타이어 수명)
-	public int accumulatedRotation;		//누적회전수
-	public String location;       				//타이어의 위치
 
-	//생성자
+	public int maxRotation;     		// 최대회전수(타이어 수명)
+	public int accumulatedRotation;		// 누적회전수
+	public String location;       		// 타이어의 위치
+
 	public Tire() {}
 	public Tire(String location, int maxRotation) {
 		this.location = location;
@@ -26,7 +25,7 @@ public class Tire {
 			System.out.println("*** " + location + " Tire 펑크 ***");
 			return false;
 		}
-	} // end roll()
+	}
 	
-} //end class
+}
 
